f023cb5c5485d21d00cec180636c04ba26b58ac1
osama5d <109868580+osama5d@users.noreply.github.com>
2023-02-23 01:25:05 +0300

    Add files via upload

---
+++ Python_Caculator.py
@@ -0,0 +1,20 @@
+# 1- input first numebr.
+first_numebr = int(input("enter first number: "))
+
+# 2- input operation type.
+operation = input("enter operation type: ")
+
+# 3- input second number.
+second_numebr = int(input("enter second number: "))
+
+# 4- print the result
+if operation == "+":
+  print(first_numebr + second_numebr)
+elif operation == "-":
+  print(first_numebr - second_numebr)
+elif operation == "*":
+  print(first_numebr * second_numebr)
+elif operation == "/":
+  print(first_numebr / second_numebr)
+else:
+  print("Error!!!")
